b281d94af6dca0e4957c13bd58ed7933469f1e1f
Philippe Charrière <pcharriere@gitlab.com>
2020-07-11 18:33:51 +0200

    🔐 token

--- src/main/kotlin/garden/bots/scarlet/defineRoutes.kt
+++ src/main/kotlin/garden/bots/scarlet/defineRoutes.kt
@@ -1,14 +1,30 @@
 package garden.bots.scarlet
 
 import garden.bots.scarlet.data.Function
+import io.vertx.ext.web.LanguageHeader
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.handler.StaticHandler
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.jsonArrayOf
 import io.vertx.kotlin.core.json.obj
 
-fun createRoutes(router: Router, functions: MutableMap<String, Function>) {
+fun checkAdminToken(adminToken: String, context: RoutingContext): Result<String> {
+
+  val check: Boolean = adminToken.isBlank() || context.request().getHeader("SCARLET_ADMIN_TOKEN") == adminToken
+
+  return when(check) {
+    false -> {
+      Result.failure(Exception("😡 bad admin token"))
+    }
+    true -> {
+      Result.success(adminToken)
+    }
+  }
+}
+
+fun createRoutes(router: Router, functions: MutableMap<String, Function>, adminToken: String) {
   router.route().handler(BodyHandler.create())
 
   router.route("/*").handler(StaticHandler.create().setCachingEnabled(false))
@@ -16,84 +32,139 @@ fun createRoutes(router: Router, functions: MutableMap<String, Function>) {
   // add a function
   router.post("/functions").handler { context ->
 
-    val defaultFunctionLanguage = "js"
-    val defaultFunctionName = "hello"
-
-    val defaultFunctionCode = """
-      function hello(params) {
-        return {
-          message: "👋 Hello World 🌍",
-        }
-      }
-      """.trimIndent()
-
-
-    val params = context.bodyAsJson
-    //TODO: check structure of params
-    val functionName = params.getString("name") ?: defaultFunctionName
-    val functionLanguage = params.getString("language") ?:defaultFunctionLanguage
-    val functionCode = params.getString("code") ?: defaultFunctionCode
-    val functionVersion = params.getString("version") ?: "0.0.0"
-
-    val currentFunction: Function = Function(
-      functionName,
-      functionLanguage,
-      functionCode,
-      functionVersion
-    )
-
-    compileFunction(functionCode, functionLanguage).let { result ->
+    checkAdminToken(adminToken, context).let { tockenCheck ->
       when {
-        result.isFailure -> { // compilation error
+        /* === 😡 Failure === */
+        tockenCheck.isFailure -> {
           context.response().putHeader("content-type", "application/json;charset=UTF-8")
             .end(
               json {
-                obj("error" to result.exceptionOrNull()?.message)
+                obj("error" to "😡 bad token")
               }.encodePrettily()
             )
         }
-        result.isSuccess -> { // compilation is OK
-          //functions.put("$functionName:$functionVersion", currentFunction)
-          functions.put(functionName, currentFunction)
-          context.response().putHeader("content-type", "application/json;charset=UTF-8")
-            .end(result.getOrNull().toString())
+        /* === 🙂 Success === */
+        tockenCheck.isSuccess -> {
+          val defaultFunctionLanguage = "js"
+          val defaultFunctionName = "hello"
+
+          val defaultFunctionCode = """
+            function hello(params) {
+              return {
+                message: "👋 Hello World 🌍",
+              }
+            }
+          """.trimIndent()
+
+          val params = context.bodyAsJson
+          //TODO: check structure of params
+          val functionName = params.getString("name") ?: defaultFunctionName
+          val functionLanguage = params.getString("language") ?:defaultFunctionLanguage
+          val functionCode = params.getString("code") ?: defaultFunctionCode
+          val functionVersion = params.getString("version") ?: "0.0.0"
+
+          val currentFunction: Function = Function(
+            functionName,
+            functionLanguage,
+            functionCode,
+            functionVersion
+          )
+
+          compileFunction(functionCode, functionLanguage).let { result ->
+            when {
+              /* === 😡 Failure === */
+              result.isFailure -> { // compilation error
+                context.response().putHeader("content-type", "application/json;charset=UTF-8")
+                  .end(
+                    json {
+                      obj("error" to result.exceptionOrNull()?.message)
+                    }.encodePrettily()
+                  )
+              }
+              /* === 🙂 Success === */
+              result.isSuccess -> { // compilation is OK
+                //functions.put("$functionName:$functionVersion", currentFunction)
+                functions.put(functionName, currentFunction)
+                context.response().putHeader("content-type", "application/json;charset=UTF-8")
+                  .end(result.getOrNull().toString())
+              }
+            }
+          }
         }
       }
     }
+
   }
 
   // execute function
   // add version ? How to deal with version (create an unique ID)
   router.post("/execute/:function_name").handler { context ->
-    val params = context.bodyAsJson
-    //TODO: check structure of params
-    // call the function
-    val functionName=context.request().getParam("function_name")
-    invokeFunction(
-      functionName,
-      params,
-      functions.get(functionName)?.language
-    ).let { result ->
+
+    checkAdminToken(adminToken, context).let {tockenCheck ->
       when {
-        result.isFailure -> { // execution error
+        /* === 😡 Failure === */
+        tockenCheck.isFailure -> {
           context.response().putHeader("content-type", "application/json;charset=UTF-8")
             .end(
               json {
-                obj("error" to result.exceptionOrNull()?.message)
+                obj("error" to "😡 bad token")
               }.encodePrettily()
             )
         }
-        result.isSuccess -> { // execution is OK
-          context.response().putHeader("content-type", "application/json;charset=UTF-8")
-            .end(result.getOrNull().toString())
-        }
-      }
-    }
+        /* === 🙂 Success === */
+        tockenCheck.isSuccess -> {
+          val params = context.bodyAsJson
+          //TODO: check structure of params
+          // call the function
+          val functionName=context.request().getParam("function_name")
+          invokeFunction(
+            functionName,
+            params,
+            functions.get(functionName)?.language
+          ).let { result ->
+            when {
+              /* === 😡 Failure === */
+              result.isFailure -> { // execution error
+                context.response().putHeader("content-type", "application/json;charset=UTF-8")
+                  .end(
+                    json {
+                      obj("error" to result.exceptionOrNull()?.message)
+                    }.encodePrettily()
+                  )
+              }
+              /* === 🙂 Success === */
+              result.isSuccess -> { // execution is OK
+                context.response().putHeader("content-type", "application/json;charset=UTF-8")
+                  .end(result.getOrNull().toString())
+              }
+            }
+          }
+        } // end of isSuccess
+      } // end of when
+    } // end of let
+
   }
 
   router.get("/functions").handler { context ->
-    context.response().putHeader("content-type", "application/json;charset=UTF-8")
-      .end(jsonArrayOf(functions).encodePrettily())
+
+    checkAdminToken(adminToken, context).let { tockenCheck ->
+      when {
+        /* === 😡 Failure === */
+        tockenCheck.isFailure -> {
+          context.response().putHeader("content-type", "application/json;charset=UTF-8")
+            .end(
+              json {
+                obj("error" to "😡 bad token")
+              }.encodePrettily()
+            )
+        }
+        /* === 🙂 Success === */
+        tockenCheck.isSuccess -> {
+          context.response().putHeader("content-type", "application/json;charset=UTF-8")
+            .end(jsonArrayOf(functions).encodePrettily())
+        }
+      }
+    }
   }
 
   // TODO add health check etc ...
