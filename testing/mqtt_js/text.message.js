8498da6f8a407facc6960fd47ea12a903137b17f
k33G <pcharriere@gitlab.com>
2021-01-20 05:36:32 +0100

    📝 change the way oh handling format message

--- testing/mqtt_js/text.message.js
+++ testing/mqtt_js/text.message.js
@@ -4,13 +4,12 @@ const client  = mqtt.connect('mqtt://localhost:1883')
 client.on('connect', _ => {
   client.subscribe('buddies', (err) => {
     if (!err) {
-      client.publish('buddies', JSON.stringify({message:"👋 hello world"}))
       client.publish('buddies', "hey people")
     }
   })
 })
 
 client.on('message',  (topic, message) => {
-  console.log("👋",JSON.parse(message.toString()))
+  console.log("👋", message.toString())
   client.end()
 })
